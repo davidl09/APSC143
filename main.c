8eb70fa0390c99f634b5fb5c373c5567bb2f6c82
davidl09 <114118694+davidl09@users.noreply.github.com>
2022-11-11 19:47:27 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,57 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-double getUserInputDouble();
-double getTaylor(double value, int terms);
-double getRealValue(double pow_e);
-int getUserInputInt();
-int factorial(int anInt);
+double getRSum(double a, double b, int n);
+double getUserInput();
 
 int main() {
-    while(1){
-        printf("Enter the number of terms of the Taylor approximation\n");
-        int numOfTerms = getUserInputInt();
-        printf("Enter the value to be computed using the taylor approximation\n");
-        double x = getUserInputDouble();
-        double taylorApproximation = getTaylor(x, numOfTerms);
-        double realValue = getRealValue(x);
-        printf("The value of e^x^2 at %.2lf is approximately %.10lf after adding %d terms. \nThe actual value of the function is %.10lf.\n\n",
-               x, taylorApproximation, numOfTerms, realValue);
+printf("Enter your values for lower and upper bounds of integration as well as number of rectangles.\nLower bound:\n");
+double lowerBound = getUserInput();
+printf("Upper bound:\n");
+double upperBound = getUserInput();
+printf("Number of rectangles:\n");
+int numOfRectangles = getUserInput();
+double Area = getRSum(lowerBound, upperBound, numOfRectangles);
+printf("The area under the curve e^(x^2) from %lf to %lf is approximately %lf for %d rectangles\n", lowerBound, upperBound, Area, numOfRectangles);
+}
+
+double getRSum(double a, double b, int n){
+    double recArray[n];
+    double dx = 0;
+    double sum = 0;
+    double xi = 0;
+    dx = (b-a)/n;
+    for(int i = 0; i<n; i++){
+        xi = a + i * dx;
+        recArray[i] = dx*(exp((xi*xi)));
     }
-}
-
-double getRealValue(double pow_e){
-    double realVal = exp((pow_e*pow_e));
-    return realVal;
-}
-
-double getUserInputDouble(){
-    double userInputD;
-    scanf("%lf", &userInputD);
-    return userInputD;
-}
-
-int getUserInputInt(){
-    int userInputI;
-    scanf("%d", &userInputI);
-    return userInputI;
-}
-
-double getTaylor(double value, int terms){
-    double taylorApprox = 1;
-    for(int i = 1; i < terms; i++){
-        taylorApprox = taylorApprox + (pow((pow(value, 2)), i))/factorial(i); //taylor series: inf sum of ((x^2)^n)/n! starting at n=0.
+    for(int i = 0; i<n; i++){
+        sum += recArray[i];
     }
-    return taylorApprox;
+    return sum;
 }
 
-int factorial(int anInt){
-    int factorialOfAnInt = anInt;
-    if(anInt==0)
-        factorialOfAnInt = 1;
-    else{for(int i = anInt-1; i>0; i--){
-            factorialOfAnInt = factorialOfAnInt*i;
-        }
-    }
-    return factorialOfAnInt;
+double getUserInput(){
+    double aNum;
+    scanf("%lf", &aNum);
+    return aNum;
 }
